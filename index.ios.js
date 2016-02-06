7fa9e1c3b41bf53ecc98fb657860823ec904fa8e
santhosbaala <rssanthosbaala@gmail.com>
2016-02-06 13:32:43 +0530

    :gem: Login scene.

--- index.ios.js
+++ index.ios.js
@@ -1,38 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
 import React, {
   AppRegistry,
-  Component,
   StyleSheet,
   Text,
-  View
+  View,
+  Navigator,
 } from 'react-native';
 
-class HappySplit extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.appName}>
-          HappySplit
-        </Text>
-      </View>
-    );
-  }
-}
+import Login from './scenes/login/login';
+
+const LOGIN_SCREEN = 'login';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#fefefe',
   },
-  appName: {
-    fontSize: 24,
-    color: 'rgb(239, 74, 29)',
+});
+
+const HappySplit = React.createClass({
+  renderScene: function renderScene(route) {
+    if (route.name === LOGIN_SCREEN) {
+      return <Login />;
+    }
+
+    return <Text>{route.name}</Text>;
+  },
+
+  render: function render() {
+    return (
+      <View style={styles.container}>
+        <Navigator
+          initialRoute={{name: LOGIN_SCREEN, index: 0}}
+          renderScene={this.renderScene}
+        />
+      </View>
+    );
   },
 });
 
---
+++ scenes/login/login.ios.js
@@ -0,0 +1,94 @@
+// TODO: Figure out how to autoscroll on keyboard entry
+import React, {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableHighlight,
+  TouchableOpacity,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoBox: {
+    width: 100,
+    height: 100,
+    marginBottom: 30,
+    borderRadius: 5,
+    backgroundColor: '#000',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoBoxText: {
+    fontSize: 10,
+    color: '#fefefe',
+  },
+  loginBox: {
+    width: 280,
+    marginBottom: 150,
+    flexDirection: 'column',
+  },
+  textInput: {
+    height: 40,
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  loginButtonContainer: {
+    height: 40,
+    backgroundColor: '#000000',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loginButtonContainerText: {
+    fontSize: 20,
+    color: '#fefefe',
+  },
+  signUpButtonContainer: {
+    marginTop: 12,
+  },
+  signUpButtonText: {
+    fontSize: 14,
+    textAlign: 'center',
+  },
+});
+
+const Login = React.createClass({
+  render: function render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.logoBox}>
+          <Text style={styles.logoBoxText}>LOGO</Text>
+        </View>
+        <View style={styles.loginBox}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoFocus
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Password"
+            secureTextEntry
+            autoCorrect={false}
+          />
+          <TouchableHighlight>
+            <View style={styles.loginButtonContainer}>
+              <Text style={styles.loginButtonContainerText}>Login</Text>
+            </View>
+          </TouchableHighlight>
+          <TouchableOpacity style={styles.signUpButtonContainer}>
+            <Text style={styles.signUpButtonText}>New? Sign Up!</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  },
+});
+
+export default Login;
